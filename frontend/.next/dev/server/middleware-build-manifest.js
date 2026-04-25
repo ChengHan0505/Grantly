3b66023717c8e495d3a6e91fd0b6a59c2864ff16
Kai-nf <lookkaiyang2005@gmail.com>
2026-04-26 03:10:19 +0800

    Backend frontend integration

--- frontend/.next/dev/server/middleware-build-manifest.js
+++ frontend/.next/dev/server/middleware-build-manifest.js
@@ -4,7 +4,7 @@ globalThis.__BUILD_MANIFEST = {
   },
   "devFiles": [],
   "polyfillFiles": [
-    "static/chunks/06nw_next_dist_build_polyfills_polyfill-nomodule.js"
+    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
   ],
   "lowPriorityFiles": [
     "static/development/_buildManifest.js",
@@ -12,15 +12,15 @@ globalThis.__BUILD_MANIFEST = {
     "static/development/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0~w7spg._.js",
-    "static/chunks/06nw_next_dist_compiled_next-devtools_index_13hj2s2.js",
-    "static/chunks/06nw_next_dist_compiled_react-dom_0mzockq._.js",
-    "static/chunks/06nw_next_dist_compiled_react-server-dom-turbopack_03-bk1j._.js",
-    "static/chunks/06nw_next_dist_compiled_133zw3q._.js",
-    "static/chunks/06nw_next_dist_client_0n2hh3r._.js",
-    "static/chunks/06nw_next_dist_0fzo3ea._.js",
-    "static/chunks/06nw_@swc_helpers_cjs_0i31w9z._.js",
-    "static/chunks/cursor_SME(UMHACK)_Grantly_frontend_0rqeker._.js",
-    "static/chunks/turbopack-cursor_SME(UMHACK)_Grantly_frontend_0yk51bn._.js"
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_10z625~._.js",
+    "static/chunks/node_modules_next_dist_compiled_next-devtools_index_0553esy.js",
+    "static/chunks/node_modules_next_dist_compiled_react-dom_058-ah~._.js",
+    "static/chunks/node_modules_next_dist_compiled_react-server-dom-turbopack_0p3wegg._.js",
+    "static/chunks/node_modules_next_dist_compiled_0rpq4pf._.js",
+    "static/chunks/node_modules_next_dist_client_0fhqo1d._.js",
+    "static/chunks/node_modules_next_dist_115brz8._.js",
+    "static/chunks/node_modules_@swc_helpers_cjs_0-4ujiy._.js",
+    "static/chunks/_0rqeker._.js",
+    "static/chunks/turbopack-_0p44nws._.js"
   ]
 };
